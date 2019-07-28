1c22ae382609226e3e89af9ea47026c7649f01a2
Michael Adair <riada125@hotmail.com>
2019-07-28 22:52:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ function theBeatlesPlay(musicians, instruments) {
 }
 
 function johnLennonFacts(facts) {
-  const shoutedFacts = []
+  var shoutedFacts = []
 
   let i = 0
   while (i < facts.length) {
